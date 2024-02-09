8663c98ccfad9a499afe2b233c2618d3dcfbf7ca
Steph01 <roy.guevara2903@gmail.com>
2024-02-09 14:19:50 -0600

    Agregando

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 let notes = [
     {
